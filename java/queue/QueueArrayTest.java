d6b87f049d708139b49adeffc30c6c735d69581e
vladimirmovsesan <vladimirmovsesan-dev@yandex.ru>
2022-03-01 03:38:37 +0300

    ArrayQueue solution modified

--- java/queue/QueueArrayTest.java
+++ java/queue/QueueArrayTest.java
@@ -2,6 +2,16 @@ package queue;
 
 public class QueueArrayTest {
     public static void main(String[] args) {
-        
+        ArrayQueue queue1 = new ArrayQueue();
+        ArrayQueue queue2 = new ArrayQueue();
+        for (int i = 0; i < 20; i++) {
+            queue1.enqueue(i + 1);
+            queue2.enqueue(i + 1);
+        }
+
+        for (; !queue1.isEmpty() || !queue2.isEmpty();) {
+            System.out.println(queue1.size() + " " + queue1.dequeue());
+            System.out.println(queue2.size() + " " + queue2.dequeue());
+        }
     }
 }
