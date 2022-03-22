3f60446e105d50e1a96e03ba6ad505d165c36e69
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2022-03-22 13:37:25 +0300

    + PieIffAbs variant for JS Functions HW

--- javascript/jstest/expression/Operations.java
+++ javascript/jstest/expression/Operations.java
@@ -2,6 +2,7 @@ package jstest.expression;
 
 import java.util.Arrays;
 import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
 
 /**
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
@@ -15,6 +16,23 @@ public interface Operations {
         checker.any("*", "Multiply", 0, 2, arith(1, (a, b) -> a * b));
         checker.any("/", "Divide", 1, 2, arith(1, (a, b) -> a / b));
     };
+
+    Operation PI = constant("pi", Math.PI);
+    Operation E = constant("e", Math.E);
+    Operation ABS = unary("abs", "Abs", Math::abs, null);
+    Operation IFF = fixed("iff", "Iff", 3, args -> args[0] >= 0 ? args[1] : args[2], null);
+
+    private static Operation constant(final String name, final double value) {
+        return checker -> checker.constant(name, value);
+    }
+
+    private static Operation unary(final String name, final String alias, final DoubleUnaryOperator op, final int[][] simplifications) {
+        return checker -> checker.unary(name, alias, op, simplifications);
+    }
+
+    private static Operation fixed(final String name, final String alias, final int arity, final BaseTester.Func f, final int[][] simplifications) {
+        return checker -> checker.fixed(name, alias, arity, f, simplifications);
+    }
 
     private static BaseTester.Func arith(final double zero, final DoubleBinaryOperator f) {
         return args -> args.length == 0 ? zero
--- javascript/jstest/functional/FunctionalTest.java
+++ javascript/jstest/functional/FunctionalTest.java
@@ -3,6 +3,8 @@ package jstest.functional;
 import base.Selector;
 import jstest.expression.Builder;
 
+import static jstest.expression.Operations.*;
+
 /**
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
  */
@@ -24,6 +26,7 @@ public final class FunctionalTest {
 
     public static final Selector SELECTOR = selector()
             .variant("Base")
+            .variant("PieIffAbs",   PI,   E,    IFF,      ABS)
             .selector();
 
     public static void main(final String... args) {
